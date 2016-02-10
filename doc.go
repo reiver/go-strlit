6777259aa02d7ef1d4d522feae14672e0d8851c1
Charles Iliya Krempeaux <iamreiver@gmail.com>
2016-02-10 07:42:30 -0800

    added some more docs

---
+++ doc.go
@@ -0,0 +1,21 @@
+/*
+Package strlit provides a way to "compile" a string literal into the value the it represents.
+
+For example, here are some string different string literals:
+
+'apple banana cherry'
+
+"apple banana cherry"
+
+‘apple banana cherry’
+
+“apple banana cherry”
+
+‹apple banana cherry›
+
+«apple banana cherry»
+
+Note that 6 different styles of quotes are supported by this package.
+
+*/
+package strlit
